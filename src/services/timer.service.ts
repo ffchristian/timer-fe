b713be7619aa3093530e7ad05673e77edfee3116
Christian Farias <christian.farias@mecuris.com>
2021-07-06 19:27:45 +0200

    add service test I

---
+++ src/services/timer.service.ts
@@ -0,0 +1,25 @@
+
+interface ITimerRequest {
+  archiveUrl: string,
+  archived: boolean,
+  forks: number
+}
+export default class TimerService {
+  private apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+
+  async getTimerData(): Promise<[ITimerRequest]> {
+    const data = await fetch(this.apiUrl);
+    const timerRequest = (await data.json()).map( (obj: any) => {
+      return {
+        archiveUrl: obj.archive_url,
+        archived: obj.archived,
+        forks: obj.forks,
+      }
+    });
+    return timerRequest;
+  }
+  async saveTimerData() {
+    const data = await fetch(this.apiUrl);
+    return await data.json();
+  }
+}
